1db668df588d5cf73f0dede9a24a8f53ef05146d
zikbro005 <127200839+zikbro005@users.noreply.github.com>
2023-05-09 12:29:38 +0200

    Add files via upload

---
+++ Giocatori.java
@@ -0,0 +1,13 @@
+public class Giocatori{
+private  Giocatori  G[];
+public Giocatori(Giocatori G[]){
+setG(G);
+}
+
+public void setGiocatori(Giocatori G[]){
+    this.Giocatori=Giocatori;
+}
+public Giocatori[] getGiocatori(){
+    
+    return Giocatori;
+}
