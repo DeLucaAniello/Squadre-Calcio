c5a6c6359a827aaa55085ffd14ba33419630c585
Francesco Pellegrino <127199306+iTzFrancy@users.noreply.github.com>
2023-05-24 09:25:33 +0200

    Add files via upload

---
+++ squadra.java
@@ -0,0 +1,12 @@
+public class Squadra {
+private  Giocatori  G[];
+public Squadra(Giocatori G[]){
+setG(G);
+}
+public void setSquadra(Giocatori G[]){
+this.G=G;
+}
+public Squadra[] getGiocatori(){
+return G;
+}
+}
